f6707f5ce3313afc7c4e7c6b6013e29b1bce7503
Aman Singh <as2211075@gmail.com>
2024-03-15 23:36:58 +0530

    ~hamburger menu
    added the hamburger menu animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+var head = document.querySelector("header");
+var overlay = document.querySelector(".overlay");
+// ham.addEventListener("click", ham_toggle)
+
+function ham_toggle() {
+  head.classList.toggle("open");
+
+  // if (head.classList.contains("open")) {
+  //   overlay.classList.remove("fade-out");
+  //   overlay.classList.add("fade-in");
+  // } else {
+  //   overlay.classList.remove("fade-in");
+  //   overlay.classList.add("fade-out");
+  // }
+}
